2cf1a0a476daddc82690bb5a6475714a14835fc8
Luis Rodriguez <luis@startup-aurora.com>
2022-03-11 21:57:43 -0600

    modified title (#1)

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Dev to site!
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
