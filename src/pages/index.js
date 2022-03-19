4a371d300d6beacf7b1f76fdb319389183efb8d8
Luis Francisco Rodriguez <luis@startup-aurora.com>
2022-03-19 15:50:28 -0600

    added home and posts section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import Layout from '../components/Layout/Layout';
-
+import Posts from '../components/Posts';
 function Home() {
-  return <Layout> Home</Layout>;
+  const [posts, setPost] = React.useState([]);
+
+  React.useEffect(() => {
+    async function getAllPosts() {
+      const posts = await fetch(
+        'https://luis-kodemia-devto-api.vercel.app/api/posts'
+      ).then((res) => res.json());
+      setPost(posts.data);
+    }
+    getAllPosts();
+  }, []);
+
+  return (
+    <Layout>
+      <Row>
+        <Col>Left</Col>
+        <Col md={6}>
+          {posts.map((post) => (
+            <Posts key={post._id} />
+          ))}
+        </Col>
+        <Col>Right</Col>
+      </Row>
+    </Layout>
+  );
 }
 
 export default Home;
