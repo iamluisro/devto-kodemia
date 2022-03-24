2c87ff4283a01cbe19d447f3e5116855d6da57d2
Luis Francisco Rodriguez <luis@startup-aurora.com>
2022-03-24 01:51:37 -0600

    getting post data and writer data

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 
-function Posts() {
+function Posts(props) {
+  const {data} = props
+  const {image, title, content, writer} = data
+  console.log("🚀 ~ file: index.js ~ line 5 ~ Posts ~ image", image)
   return (
     <div>
       <Card className='m-3'>
         <Card.Img
           variant='top'
-          src='https://res.cloudinary.com/practicaldev/image/fetch/s--nb36aYKf--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bmhenvulkzg8as4smqgi.png'
+          alt='cover-post'
+          src={image}
         />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
+         <img src="" className="rounded-circle avatar" alt="Avatar user" />;
+
+          <Card.Title>{title}</Card.Title>
         </Card.Body>
       </Card>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import { Link } from 'react-router-dom';
-import Posts from '../components/Posts';
+import Card from 'react-bootstrap/Card';
+
 function Home() {
   const [posts, setPost] = React.useState([]);
 
@@ -20,12 +21,42 @@ function Home() {
     <div>
       <Row>
         <Col>Left</Col>
-        <Col md={6}>
-          {posts.map((post) => (
-            <Link key={post._id} to={`/posts/${post._id}`}>
-              <Posts  />
-            </Link>
-          ))}
+        <Col xs={12} md={7}>
+          {posts.map((post) => {
+            const { _id, image, title, writer, createdAt } = post;
+            const { username, name, lastName } = writer.length > 0 && writer[0];
+            const img =
+              image ||
+              'https://res.cloudinary.com/practicaldev/image/fetch/s--UbduOPa1--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2g86wjqt9ce7qplixp7i.png';
+            return (
+              <Link
+                key={_id}
+                to={`/${username}/${_id}`}
+                style={{ textDecoration: 'none' }}
+              >
+                <Card className='m-3'>
+                  <Card.Img variant='top' alt='cover-post' src={img} />
+                  <Card.Body>
+                    <div className='d-flex'>
+                      <img
+                        src='https://ca.slack-edge.com/TCRFJBKB6-U02KCUW0Q3H-3ad9c2dbe014-512'
+                        className='rounded-circle me-2'
+                        style={{ width: '2rem', height: '2rem' }}
+                        alt='Avatar user'
+                      />
+                      <div className='d-flex flex-column align-items-start'>
+                        <p className='m-0'>
+                          {name} {lastName}
+                        </p>
+                        <p className='text-muted'>{createdAt}</p>
+                      </div>
+                    </div>
+                    <Card.Title className=''>{title}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Link>
+            );
+          })}
         </Col>
         <Col>Right</Col>
       </Row>
