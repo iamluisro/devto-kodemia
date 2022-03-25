4d20f35b33912e0fbfaad17a6b1dc6c10ee8ff9d
Luis Rodriguez <luis@startup-aurora.com>
2022-03-25 01:50:21 -0600

    Getposts (#9)
    * getting post data and writer data
    
    * added prettier and create account page
    
    * added scss variables and create account pg
    
    * fixed enppoint

--- src/components/layout-with-children/index.js
+++ src/components/layout-with-children/index.js
@@ -6,43 +6,44 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import FormControl from 'react-bootstrap/FormControl';
 import InputGroup from 'react-bootstrap/InputGroup';
+import './Layout.scss'
 
 function NavBar() {
   return (
-    <Navbar bg='light' expand='lg'>
-      <Container fluid className='justify-content-between'>
-        <div className='d-flex'>
-          <Link to='/'>
-            <Navbar.Brand >Devto</Navbar.Brand>
+    <Navbar expand="lg" variant='light' bg="white">
+      <Container fluid className="justify-content-between">
+        <div className="d-flex">
+          <Link to="/">
+            <Navbar.Brand>Devto</Navbar.Brand>
           </Link>
-          <InputGroup className='mb-3'>
+          <InputGroup className="mb-3">
             <FormControl
-              placeholder='Search...'
-              aria-label='Search'
-              aria-describedby='basic-addon1'
+              placeholder="Search..."
+              aria-label="Search"
+              aria-describedby="basic-addon1"
             />
             <InputGroup.Text
-              id='basic-addon1'
+              id="basic-addon1"
               style={{ cursor: 'pointer' }}
               onClick={() => alert('hello')}
             >
-              <i className='bi bi-search'></i>
+              <i className="bi bi-search"></i>
             </InputGroup.Text>
           </InputGroup>
         </div>
         <div>
-          <Navbar.Toggle aria-controls='navbarScroll' />
-          <Navbar.Collapse id='navbarScroll'>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
             <Nav
-              className='me-auto my-2 my-lg-0'
+              className="me-auto my-2 my-lg-0"
               style={{ maxHeight: '100px' }}
               navbarScroll
             >
-              <Button variant='outline-primary' className='me-2'>
+              <Button variant="outline-primary" className="me-2">
                 Sign In
               </Button>
-              <Link to='/create-account'>
-                <Button variant='outline-primary'>Create Account</Button>
+              <Link to="/create-account">
+                <Button variant="outline-primary">Create Account</Button>
               </Link>
             </Nav>
           </Navbar.Collapse>
@@ -54,15 +55,13 @@ function NavBar() {
 
 function LayoutWChildren({ children }) {
   return (
-    <div className='Layout-container'>
-      <NavBar>Header</NavBar>
+    <div className="Layout-container bg-custom">
+      <NavBar />
       <Container>
-        <main>
-          {children}
-        </main>
+        <main>{children}</main>
       </Container>
       <footer
-        className='d-flex border border-1 justify-content-center'
+        className="d-flex border border-1 justify-content-center"
         style={{ height: '250px' }}
       >
         footer
--- src/pages/writer-page.js
+++ src/pages/writer-page.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function WriterPage() {
-    return(<div>This is the writer page</div>)
+  return <div>This is the writer page</div>;
 }
 
 export default WriterPage;
